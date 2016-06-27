9b2a6ea71743b38fc9f7b7a6666509340f5ba989
Phillip Jones <phillipemersonjones@gmail.com>
2016-06-27 12:33:15 -0500

    added the micropost scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627161130) do
+ActiveRecord::Schema.define(version: 20160627173138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
